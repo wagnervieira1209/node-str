7574a0322ba50788dbfa7b8afbfadac319988d99
Wagner Vieira <47631966+wagnervieira1209@users.noreply.github.com>
2022-02-25 07:15:46 -0300

    Delete config.js

--- src/config.js
+++
@@ -1,8 +0,0 @@
-global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
-global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
-
-module.exports = {
-    connectionString: 'mongodb://localhost:27017/node-str',
-    sendgridKey: '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',
-    containerConnectionString: 'DefaultEndpointsProtocol=https;AccountName=wagnervieira;AccountKey=pVwf1NqJWpRLfMIqki8qvxarMFMUjhVziPytqn8KHltpQfX7k5ohiF/DDdUejrplf+thoGoq8iupxu1NQmXWIQ==;EndpointSuffix=core.windows.net'
-}
